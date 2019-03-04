c515ae2a96db4a475c2942e61fd1e4ea0df89480
Alex MacDonald <alexjmacdonald@users.noreply.github.com>
2019-03-04 12:49:33 -0500

    Implement maploc(), to make copies to a secondary buffer respect cache locality

--- main.cpp
+++ main.cpp
@@ -7,7 +7,174 @@
 #include <vector>
 #include <typeinfo> // for typeid debugging
 
+#include <assert.h>
+
+// global TODOs:
+// - replace short/int with sized types u8 u32 etc
+// - move testing to cmake
+// - cmake should determine grid size, row width, col width
+
+template <unsigned int GRID, unsigned int _ROW_WIDTH, unsigned int _COL_WIDTH>
+unsigned int maploc(unsigned int r, unsigned int c) {
+  // TODO validation strategy,
+  // right now we assume the caller isn't going to send OOB coords
+  // this is probably appropriate (avoid branching) but should circle back
+
+  // If the grid size doesn't evenly divide,
+  // we simply ignore the margin.
+  constexpr auto ROW_WIDTH = _ROW_WIDTH - (_ROW_WIDTH % GRID);
+  constexpr auto COL_WIDTH = _COL_WIDTH - (_COL_WIDTH % GRID);
+  // Consider a 3x3 grid over 9x9 pixels:
+  /*
+    * a b c  x x x  x x x
+    * d e f  x x x  x x x
+    * g h i  x x x  x x x
+    *
+    * x x x  x x x  x x x
+    * x x x  x x x  x x x
+    * x x x  x x x  x x x
+    *
+    * x x x  x x x  x x x
+    * x x x  x x x  x x x
+    * x x x  x x x  x x x
+    */
+  // For a regular linear traversal:
+  // [ a b c . . . . . . d e f ... ]
+  // the members of a cell aren't consecutive.
+  //
+  // For efficiency's sake (maintaining cache coherency & avoiding needless reiteration)
+  // we make one pass across the image, copying pixels to a new buffer, and mapping
+  // them to new locations in the buffer so that we get:
+  // [ a b c d e f g h i . . . . . . ... ]
+  //
+  // The formula for this looks like:
+  //
+  /*
+    *  (using ordinal ranks, not 0-based indexing)
+    *
+    *  Presume we map the contents of each grid cell
+    *  to its grid's new index in the new buffer:
+    *
+    *    x- - - - - - - - - - - - - -
+    *  y
+    *  |  1  1  1   2  2  2   3  3  3
+    *  |  1  1  1   2  2  2   3  3  3
+    *  |  1  1  1   2  2  2   3  3  3
+    *
+    *  |  4  4  4   5  5  5   6  6  6
+    *  |  4  4  4   5  5  5   6  6  6
+    *  |  4  4  4   5  5  5   6  6  6
+    *
+    *  and then we account for the grid size
+    *  (so, 1+0, 1+9, 1+9+9...)
+    *  so that each cell maps to the grid's actual mem index:
+    *
+    *    x- - - - - - - - - - - - - -
+    *  y
+    *  |  1  1  1   10 10 10  19 19 19
+    *     1  1  1   10 10 10  19 19 19
+    *  |  1  1  1   10 10 10  19 19 19
+    *
+    *  |  28 28 28  37 37 37  46 46 46
+    *     28 28 28  37 37 37  46 46 46
+    *  |  28 28 28  37 37 37  46 46 46
+    *
+    *  Pretty close!
+    *
+    *  Finally, we use addition to compose our grid cell's index
+    *  with the cell element's index within the grid cell:
+    *
+    *    x- - - - - - - - - - - - - -
+    *  y
+    *  |  0  1  2   0  1  2   0  1  2
+    *     3  4  5   3  4  5   3  4  5
+    *  |  6  7  8   6  7  8   6  7  8
+    *
+    *  |  0  1  2   0  1  2   0  1  2
+    *     3  4  5   3  4  5   3  4  5
+    *  |  6  7  8   6  7  8   6  7  8
+    *
+    *  Regular piecewise matrix addition.
+    *
+    *  Therefore...
+    *
+    *    x- - - - - - - - - - - - - -
+    *  y
+    *  |  1  2  3   4  5  6   7  8  9
+    *     10 11 12  13 14 15  16 17 18
+    *  |  19 20 21  22 23 24  25 26 27
+    *
+    *  |  28 29 30  31 32 33  34 35 36
+    *     37 38 39  40 41 42  43 44 45
+    *  |  46 47 48  49 50 51  52 53 54
+    *
+    *     becomes
+    *
+    *    x- - - - - - - - - - - - - -
+    *  y
+    *  |  1  2  3   10 11 12  19 20 21
+    *     4  5  6   13 14 15  22 23 24
+    *  |  7  8  9   16 17 18  25 26 27
+    *
+    *  |  28 29 30  37 38 39  46 47 48
+    *     31 32 33  40 41 42  49 50 51
+    *  |  34 35 36  43 44 45  52 53 54
+    */
+
+    const unsigned int cells_above        = (r/GRID) * (COL_WIDTH / GRID);
+    const unsigned int cells_before       = c / GRID;
+    const unsigned int starting_grid_idx  = (GRID*GRID) * (cells_above + cells_before);
+    const unsigned int within_grid_idx    = (r%GRID)*GRID + c%GRID;
+
+    return (starting_grid_idx + within_grid_idx);
+}
+
+void tests() {
+  /*
+   * ordinal positions (subtr 1 to get zero-based idx)
+   *    x- - - - - - - - - - - - - -
+   *  y
+   *  |  1  2  3   10 11 12  19 20 21
+   *     4  5  6   13 14 15  22 23 24
+   *  |  7  8  9   16 17 18  25 26 27
+   *
+   *  |  28 29 30  37 38 39  46 47 48
+   *     31 32 33  40 41 42  49 50 51
+   *  |  34 35 36  43 44 45  52 53 54
+   */
+
+  /* for printf debugging the asserts below
+    for (int i = 0; i < 54; i++) {
+      int r = i/9;
+      int c = i%9;
+      auto x = (maploc<3, 9, 9>(r, c));
+      std::cout << ">> " << i << "\t" << "\t" << r << "/" << c << ":\t" << x << std::endl;
+    }
+  */
+
+  // basics
+  assert( (maploc<3, 9, 9>(0, 0) == 0)  );
+  assert( (maploc<3, 9, 9>(1, 0) == 3)  );
+  assert( (maploc<3, 9, 9>(1, 1) == 4)  );
+  assert( (maploc<3, 9, 9>(5, 0) == 33) );
+  assert( (maploc<3, 9, 9>(2, 8) == 26) );
+
+  // when grid doesn't evenly divide matrix
+  assert( (maploc<3, 9, 10>(0, 0) == 0)  );
+  assert( (maploc<3, 9, 10>(1, 0) == 3)  );
+  assert( (maploc<3, 9, 10>(1, 1) == 4)  );
+  assert( (maploc<3, 9, 10>(5, 0) == 33) );
+  assert( (maploc<3, 9, 10>(2, 8) == 26) );
+}
+
+unsigned short grayscale(const unsigned short r, const unsigned short g, const unsigned short b) {
+  // luminosity = 0.299R + 0.587G + 0.114B
+  return (299*r + 587*g + 114*b) / 1000;
+}
+
 int main(int argc, char** argv) {
+
+  tests();
 
   // Usage / handle CLI
   boost::program_options::variables_map vm;
@@ -18,6 +185,7 @@ int main(int argc, char** argv) {
     desc.add_options()
       ("help,h", "Help screen")
       ("files",  value<std::vector<std::string>>() -> multitoken() -> required(), "list of file paths")
+      // TODO optionally ingest list of keyframe indices
       ;
 
     store(parse_command_line(argc, argv, desc), vm);
@@ -59,6 +227,8 @@ int main(int argc, char** argv) {
   for (auto it = paths.begin(); it != paths.end(); it++) {
     using namespace cv;
 
+    // Allocate new buffer
+
     const auto &path = *it;
     Mat image = cv::imread( path, 1 );
 
@@ -76,9 +246,7 @@ int main(int argc, char** argv) {
       // TBD
 
       Mat frame;
-      Mat grey_fun_frame;
       video >> frame;
-      grey_fun_frame.create(frame.size(), frame.type());
 
       // A cv::Mat (frame) is 'continuous' when its internal representation is equivalent to one long array.
       // We process the underlying array directly, assuming this.
@@ -87,7 +255,7 @@ int main(int argc, char** argv) {
       if (frame.empty())
         break;
 
-      // TODO how to /properly/ find depth & channel ct?  this seems suboptimal
+      // How to /properly/ find depth & channel ct?  this seems suboptimal
       // some info on format defs:
       // https://docs.opencv.org/4.0.1/d1/d1b/group__core__hal__interface.html
       // https://codeyarns.com/2015/08/27/depth-and-type-of-matrix-in-opencv/
@@ -97,8 +265,6 @@ int main(int argc, char** argv) {
       std::cout << first_frame.channels() << std::endl;
       std::cout << (video.get(CAP_PROP_FORMAT)) << std::endl;  // CV_8U etc, see above docs
       */
-
-      // Default OpenCV format is BGR
 
       // https://stackoverflow.com/questions/8184053/accessing-elements-of-a-cvmat-with-atfloati-j-is-it-x-y-or-row-col/42327350#42327350
       // alleges row-major storage
@@ -114,31 +280,29 @@ int main(int argc, char** argv) {
       const unsigned char* pixel  = frame.ptr<unsigned char>(0);
       // TODO do you ever get videos with INT_MAX pixels?
       for(int pixel_idx = 0; pixel_idx < pixel_ct; pixel_idx++) {
+
         const int x = pixel_idx / col_ct;
         const int y = pixel_idx % col_ct;
-        // luminosity = 0.299R + 0.587G + 0.114B
+
+        // Default OpenCV format is BGR
         unsigned short b = static_cast<unsigned short>(*(pixel + pixel_idx*3 + 0));
         unsigned short g = static_cast<unsigned short>(*(pixel + pixel_idx*3 + 1));
         unsigned short r = static_cast<unsigned short>(*(pixel + pixel_idx*3 + 2));
-        unsigned short grey = (299*r + 587*g + 114*b) / 1000;
 
-        // TODO is an RGB triplicate format a constraint on imshow()
-        // or are there properties on the matrix / flags for display I can tweak?
-        grey_fun_frame.at<unsigned char>(x,y*3+0) = static_cast<unsigned short>(grey);
-        grey_fun_frame.at<unsigned char>(x,y*3+1) = static_cast<unsigned short>(grey);
-        grey_fun_frame.at<unsigned char>(x,y*3+2) = static_cast<unsigned short>(grey);
+        // convert pixel to grayscale
+        grayscale(r,g,b);
+        // use maploc() to copy to new buffer
+        // determine median from buffer chunks
+        //   depending on size of N,
+        //   quickselect or insertion sort?
+        //   TODO benchmark and then use GRID SIZE in template
+        //   to determine
+        // map buffer chunks to median
+        // print to stdout, achieve csv write via redirection
+
       }
-
-      std::cout << frame_idx << std::endl;
-
-      imshow( "Frame", frame_idx%20==0 ? frame : grey_fun_frame);
-
-      char c=(char)waitKey(10);
-      if(c==27)
-        break;
     }
 
-    // When everything done, release the video capture object
     video.release();
   }
 
